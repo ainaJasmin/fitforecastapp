85c186b85d1fe91d5b562e1d427767308b91a7b0
Benjamin Camilleri <camilleriben@hotmail.com>
2024-03-20 22:04:06 +0000

    api stuff

--- src/WelcomePage.jsx
+++ src/WelcomePage.jsx
@@ -42,38 +42,55 @@
 
 // export default WelcomePage;
 
-import React, { useEffect } from 'react';
+import React from 'react';
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import './WelcomePage.css';
 import MountainImg from './Mountain2.webp';
 import BlueBarImg from "./bluebar.png"
 import LogoImg from "./fitforecast logo.png"
 import searchImg from "./search logo.png"
+import { wait } from '@testing-library/user-event/dist/utils';
 
 
 function WelcomePage() {
 
-    function clickSearch(city) {
-        const apiKey = '6ba3ee5ac50af61f21d2136ac5dab42c';
-        const geocodingApiUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apiKey}`;
-    
-        let latitude, longitude;
+    let nav = useNavigate();
 
+    let moveToMain = () => {
+        nav("/main");
+    }
+
+    const [lat, setLat] = useState(0);
+    const [lon, setLon] = useState(0);
+    const [locName, setLoc] = useState('');
+
+    function searchCity() {
+        const apiKey = '6ba3ee5ac50af61f21d2136ac5dab42c';
+        const geocodingApiUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${locName}&limit=1&appid=${apiKey}`;
+    
         fetch(geocodingApiUrl)
             .then(response=>response.json())
             .then(data=> {
                 if (data.length>0) {
-                    latitude = data[0].lat;
-                    longitude = data[0].lon;
-                    
-
-
+                    console.log(data[0].lat + " " + data[0].lon);
+                    setLat(data[0].lat);
+                    setLon(data[0].lon);
+                    localStorage.setItem("lat", lat);
+                    localStorage.setItem("lon", lon);
+                    console.log(`State: lat=${lat} lon=${lon}`);
+                    console.log(`Local Storage: lat=${localStorage.getItem("lat")} lon=${localStorage.getItem("lon")}`);
                 } else {
-                    latitude = 0;
-                    longitude = 0;
+                    console.log("API Response Not As Expected");
                 }
             })
             .catch(err=>console.log(err))
     }
+
+    function handleClick() {
+        searchCity();
+    }
+
 
     return (
         <div className="BackgroundImage">
@@ -90,11 +107,11 @@ function WelcomePage() {
                             <img src={LogoImg} alt="Logo" className="FitLogo"/>
                         </div>
                         <div className="Bar">
-                            <input type="text" id="location" placeholder="Search Location" className="SearchBar"/>
+                            <input type="text" id="location" placeholder="Search Location" className="SearchBar" onChange={(e) => setLoc(e.target.value)}/>
                         </div>
 
                         <div className="Icon">
-                            <a href='/main'><img src={searchImg} alt="Search Icon" className="Search"/></a>
+                            <img src={searchImg} alt="Search Icon" className="Search" onClick={handleClick}/>
                         </div>
                     </div>
 
