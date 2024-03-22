7f79eeb858b3f2e9d3fa95508f04a156c1ce43f4
Benjamin Camilleri <camilleriben@hotmail.com>
2024-03-22 02:58:59 +0000

    rain page css

--- src/RainPage.jsx
+++ src/RainPage.jsx
@@ -41,29 +41,43 @@ function RainPage() {
             <a href="main">
                 <img src={fitforecastLogoImg} alt="Logo" className="fit-logo"/>
             </a>
-            <div className="rain-overlay1" style={{ top: '75%' }}>
+            <div className='overlays'>
+                <div className="rain-overlay1" style={{ top: '75%' }}>
+                <div className='cont'>
                 <p id="tempVal">{forecasts.list[0].main.temp}°</p>
                 <img src={temp} alt="Logo" className="humid"/>
+                </div>
             </div>
             <div className="rain-overlay2" style={{ top: '75%' }}>
+                <div className='cont'>
                 <p id="windDirVal">{forecasts.list[0].wind.deg}°</p>
                 <img src={ap} alt="Logo" className="humid"/>
+                </div>
             </div>
             <div className="rain-overlay3" style={{ top: '75%' }}>
+                <div className='cont'>
                 <p id="visibiltyVal">{forecasts.list[0].visibility}m</p>
                 <img src={eyes} alt="Logo" className="humid"/>
+                </div>
             </div>
             <div className="rain-overlay4" style={{top: '30%'}}>
+                <div className='cont'>
                 <p id="humidityVal">{forecasts.list[0].main.humidity}%</p>
                 <img src={humidImg} alt="Logo" className="humid"/>
+                </div>
             </div>
             <div className="rain-overlay5" style={{top: '30%'}}>
+                <div className='cont'>
                 <p id="chanceOfRainVal">{100 * forecasts.list[0].pop}%</p>
                 <img src={cor} alt="Logo" className="humid"/>
+                </div>
             </div>
             <div className="rain-overlay6" style={{top: '30%'}}>
+                <div className='cont'>
                 <p id="windSpeedVal">{forecasts.list[0].wind.speed}m/s</p>
                 <img src={windImg} alt="Logo" className="humid"/>
+                </div>
+            </div>
             </div>
 
         </div>
